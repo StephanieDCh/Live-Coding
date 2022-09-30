172bfb13e1ccdb0b0ec075340bfc2b305b94feaf
Stephanie Díaz Chamorro <fannycha1996@gmail.com>
2022-09-30 13:28:10 -0500

    Se agregan los archivos index, js al repositorio

---
+++ app.js
@@ -0,0 +1,23 @@
+// Escribe tu código aquí.
+
+const list = document.querySelector('ul');
+const myArray = ['insecto', 'bootcamp', 'mangos', 'reptil', 'mosca', 'escritorio'];
+console.log(filtra('libro',myArray))
+const li = document.createElement("li");
+list.appendChild(li);
+
+function filtra(bigWords, myArray){
+l1.bigWords.length;
+a=[];
+
+myArray.forEach(element => {
+    if (element.length>l1) {
+        a.push(element);
+    }else{
+
+    }
+});//foreach
+return a;
+}
+
+
